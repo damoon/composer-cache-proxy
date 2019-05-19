2a24ec79ccdfa990d6aae3dae223239677f40a9b
David Sauer <davedamoon@gmail.com>
2019-05-19 23:16:45 +0200

    hijacked call from Hirak\Prestissimo to curl

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Cito
 
+## installation
 `composer global require damoon/cito`
+
+## testing
+`rm -rf $HOME/.cache/composer/ vendor`
+`DEBUG=1 composer require amphp/http-server`
--- src/Plugin.php
+++ src/Plugin.php
@@ -18,3 +18,15 @@ class Plugin implements ComposerPlugin\PluginInterface
     {
     }
 }
+
+namespace Hirak\Prestissimo;
+
+if (!function_exists("\Hirak\Prestissimo\curl_setopt_array")) {
+    function curl_setopt_array($ch, array $options) : bool
+    {
+        if (getenv("DEBUG")) {
+            echo "hijacked: " . $options[CURLOPT_URL] . "\n";
+        }
+        return \curl_setopt_array($ch, $options);
+    }
+}
