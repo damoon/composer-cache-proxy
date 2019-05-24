78100169325bf26353f7e05a8549fb95f3514baf
David Sauer <davedamoon@gmail.com>
2019-05-24 20:48:49 +0200

    redirect requests via cito server / skip 'https://repo.packagist.org/packages.json'

--- README.md
+++ README.md
@@ -3,6 +3,10 @@
 ## installation
 `composer global require damoon/cito`
 
+## configuration
+`DEBUG=1` enables debug output
+`CITO_SERVER=http://127.0.0.1:8080` sets cito cache server
+
 ## testing
 `rm -rf $HOME/.cache/composer/ vendor`
 `DEBUG=1 composer require amphp/http-server`
--- src/Plugin.php
+++ src/Plugin.php
@@ -24,9 +24,38 @@ namespace Hirak\Prestissimo;
 if (!function_exists("\Hirak\Prestissimo\curl_setopt_array")) {
     function curl_setopt_array($ch, array $options) : bool
     {
-        if (getenv("DEBUG")) {
-            echo "hijacked: " . $options[CURLOPT_URL] . "\n";
+        if (!isset($options[CURLOPT_URL])) {
+            return \curl_setopt_array($ch, $options);
         }
+
+        if ("https://repo.packagist.org/packages.json" == $options[CURLOPT_URL]) {
+            return \curl_setopt_array($ch, $options);
+        }
+
+        if (!getenv("CITO_SERVER")) {
+            echo "cito: CITO_SERVER not configured\n";
+            return \curl_setopt_array($ch, $options);
+        }
+
+        $beginsWith = function(string $haystack, string $needle) : string {
+            return substr($haystack, 0, strlen($needle)) === $needle;
+        };
+
+        $prefixCitoServer = function($url) use ($beginsWith) {
+            if ($beginsWith($url, "http://")) {
+                return  "/http/" . substr($url, strlen("http://"));
+            } else
+            if ($beginsWith($url, "https://")) {
+                return "/https/" . substr($url, strlen("https://"));
+            }
+        };
+
+        $url = getenv("CITO_SERVER") . $prefixCitoServer($options[CURLOPT_URL]);
+        if (getenv("DEBUG")) {
+            echo "hijacked: " . $options[CURLOPT_URL] . " calling " . $url . "\n";
+        }
+        $options[CURLOPT_URL] = $url;
+
         return \curl_setopt_array($ch, $options);
     }
 }
